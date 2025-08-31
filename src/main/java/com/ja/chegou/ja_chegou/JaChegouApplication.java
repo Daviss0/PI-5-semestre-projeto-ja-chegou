f23b68c9023bdf0c1b3635471c66b8b0d3cb9748
Davi Patricio <davis.patricio1505@gmail.com>
2025-08-30 22:18:05 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/ja/chegou/ja_chegou/JaChegouApplication.java
@@ -0,0 +1,13 @@
+package com.ja.chegou.ja_chegou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaChegouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaChegouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ja/chegou/ja_chegou/JaChegouApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ja.chegou.ja_chegou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaChegouApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
